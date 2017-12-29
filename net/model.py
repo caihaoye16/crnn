f977ce5e78611616ed3cdfa458d157cdbcc02e2d
haoyecai(蔡昊野) <haoyecai@tencent.com>
2017-12-29 21:10:24 +0800

    init

--- net/model.py
+++ net/model.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import math
-from net import custom_layers
+# from net import custom_layers
 from collections import namedtuple
 from tensorflow.contrib import rnn
 
@@ -20,10 +20,10 @@ RCNNParams = namedtuple('RCNNParameters', ['ks',#kernel_size
                                          'nh',
                                          'n_rnn',
                                          'leakyRelu',
-                                          'batch_size',
-                                        'seq_length',
-                                        'input_size',
-                                        "reuse"
+                                         'batch_size',
+                                         'seq_length',
+                                         'input_size',
+                                         "reuse"
                                          ])
 
 
@@ -53,7 +53,7 @@ class CRNNNet(object):
 
 
     def __init__(self, params=None):
-        """Init the SSD net with some parameters. Use the default ones
+        """Init the net with some parameters. Use the default ones
         if none provided.
         """
         if isinstance(params, RCNNParams):
@@ -62,112 +62,155 @@ class CRNNNet(object):
             self.params = CRNNNet.default_params
 
     # ======================================================================= #
-    def net(self, inputs,width = None):
+    def net(self, inputs, is_training, width = None):
         """rcnn  network definition.
         """
-        def BLSTM(inputs,num_hidden,num_layers,seq_len,num_classes):
-            # Defining the cell
-            # Can be:
-            #   tf.nn.rnn_cell.RNNCell
-            #   tf.nn.rnn_cell.GRUCell
-            cell = tf.contrib.rnn.core_rnn_cell.LSTMCell(num_hidden, state_is_tuple=True,reuse = self.params.reuse)
 
-            # Stacking rnn cells
-            stack = tf.contrib.rnn.core_rnn_cell.MultiRNNCell([cell] * num_layers,
-                                                              state_is_tuple=True)
+        def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=False):
+            shape = input_.get_shape().as_list()
 
-            # The second output is the last state and we will no use that
-            outputs, _ = tf.nn.dynamic_rnn(cell, inputs, sequence_length=seq_len, dtype=tf.float32)#seq_len
+            with tf.variable_scope(scope or "Linear"):
+                matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
+                                         tf.truncated_normal_initializer(stddev=stddev))
+                bias = tf.get_variable("bias", [output_size],
+                    initializer=tf.constant_initializer(bias_start))
+                if with_w:
+                    return tf.matmul(input_, matrix) + bias, matrix, bias
+                else:
+                    return tf.matmul(input_, matrix) + bias
 
-            shape = tf.shape(inputs)
-            batch_s, max_timesteps = shape[0], shape[1]
+        def BLSTM(inputs, n_hidden, num_layers, num_classes, scope='blstm'):
+            # # Defining the cell
+            # # Can be:
+            # #   tf.nn.rnn_cell.RNNCell
+            # #   tf.nn.rnn_cell.GRUCell
+            # cell = tf.contrib.rnn.core_rnn_cell.LSTMCell(num_hidden, state_is_tuple=True)
 
-            # Reshaping to apply the same weights over the timesteps
-            outputs = tf.reshape(outputs, [-1, num_hidden])
+            # # Stacking rnn cells
+            # stack = tf.contrib.rnn.core_rnn_cell.MultiRNNCell([cell] * num_layers,
+            #                                                   state_is_tuple=True)
 
-            # Truncated normal with mean 0 and stdev=0.1
-            # Tip: Try another initialization
-            # see https://www.tensorflow.org/versions/r0.9/api_docs/python/contrib.layers.html#initializers
-            W = tf.Variable(tf.truncated_normal([num_hidden,
-                                                 num_classes],
-                                                stddev=0.1), name="W")
-            # Zero initialization
-            # Tip: Is tf.zeros_initializer the same?
-            b = tf.Variable(tf.constant(0., shape=[num_classes]), name="b")
+            # # The second output is the last state and we will no use that
+            # outputs, _ = tf.nn.dynamic_rnn(cell, inputs, sequence_length=seq_len, dtype=tf.float32)#seq_len
 
-            # Doing the affine projection
-            logits = tf.matmul(outputs, W) + b
+            # shape = tf.shape(inputs)
+            # batch_s, max_timesteps = shape[0], shape[1]
 
-            # Reshaping back to the original shape
-            logits = tf.reshape(logits, [batch_s, -1, num_classes])
+            # # Reshaping to apply the same weights over the timesteps
+            # outputs = tf.reshape(outputs, [-1, num_hidden])
 
-            # Time major
-            logits = tf.transpose(logits, (1, 0, 2))
-            return logits, inputs, seq_len, W, b
+            # # Truncated normal with mean 0 and stdev=0.1
+            # # Tip: Try another initialization
+            # # see https://www.tensorflow.org/versions/r0.9/api_docs/python/contrib.layers.html#initializers
+            # W = tf.Variable(tf.truncated_normal([num_hidden,
+            #                                      num_classes],
+            #                                     stddev=0.1), name="W")
+            # # Zero initialization
+            # # Tip: Is tf.zeros_initializer the same?
+            # b = tf.Variable(tf.constant(0., shape=[num_classes]), name="b")
+
+            # # Doing the affine projection
+            # logits = tf.matmul(outputs, W) + b
+
+            # # Reshaping back to the original shape
+            # logits = tf.reshape(logits, [batch_s, -1, num_classes])
+
+            # # Time major
+            # logits = tf.transpose(logits, (1, 0, 2))
+            # return logits, inputs, seq_len, W, b
 
 
-        def conv2d(inputs,i,batchNormalization = False):
-            nOut = self.params.nm[i]
-            kernel_size = self.params.ks[i]
-            stride = self.params.ss[i]
-            if i != 6:
-                net = slim.conv2d(inputs,nOut,kernel_size,stride = stride,padding="SAME",scope = 'conv_{0}'.format(i),activation_fn=None)
-            else:
-                net = slim.conv2d(inputs, nOut, kernel_size, stride=stride, padding="VALID", scope='conv_{0}'.format(i),
-                                  activation_fn=None)
+            with tf.variable_scope(scope):
+                outputs = inputs
+                for i in num_layers:
+                    with tf.variable_scope('layer_%d' %i):
+                        # Define lstm cells with tensorflow
+                        # Forward direction cell
+                        lstm_fw_cell = rnn.LSTMBlockCell(n_hidden)
+                        # Backward direction cell
+                        lstm_bw_cell = rnn.LSTMBlockCell(n_hidden)
 
-            if(batchNormalization):
-                net = slim.batch_norm(net,scope='batchnorm{0}'.format(i))
+                        outputs, _ = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, outputs, dtype=tf.float32)
+                        outputs = tf.concat(outputs, 2)
+
+                output_list = tf.unstack(outputs, 1)
+                final_output = []
+                for output in output_list:
+
+                    final_output.append(linear(output, num_classes, 'output_layer'))
+
+                    tf.get_variable_scope().reuse_variables()
+            
+                final_output = tf.stack(final_output, 1)
+
+            return final_output
+
+
+        def conv2d(inputs, nOut, kernel_size, stride=1, padding="SAME", batchNormalization=False, is_training=None, scope='conv2d'):
+            # nOut = self.params.nm[i]
+            # kernel_size = self.params.ks[i]
+            # stride = self.params.ss[i]
+            
+            net = slim.conv2d(inputs, nOut, kernel_size, stride=stride, padding=padding, scope=scope, activation_fn=None)
+
+            if batchNormalization:
+                net = slim.batch_norm(net, is_training=is_training, scope=scope+'_bn')
 
             net = tf.nn.relu(net)
             return net
 
-        with tf.variable_scope("RCNN_net",reuse=self.params.reuse):
-            net = conv2d(inputs,0)#input batch_size*32*100*3 #net batch_size *32*100*64
-            net = slim.max_pool2d(net, [2, 2], scope='pool1')#net batch_size *16*50*64
-            print("poll_0 ", net.shape)
-            net = conv2d(net,1)#batch_size*16*50*128
-            net = slim.max_pool2d(net, [2, 2], scope='pool2')#batch_size *8*25*128 后面简写b
-            print("poll_1 ", net.shape)
-            net = conv2d(net,2,True) #b *8*25*256
 
-            net = conv2d(net,3) #b*8*25*256
-            net = custom_layers.pad2d(net, pad=(0, 1))#b*8*27*256
-            net = slim.max_pool2d(net,[2,2],stride =[2,1],padding = "VALID")#b*4*26*256
+
+        with tf.variable_scope("CRNN_net",reuse=None):
+            net = conv2d(inputs, 64, 3, scope='conv1')#input batch_size*32*100*3 #net batch_size *32*100*64
+            net = slim.max_pool2d(net, [2, 2], scope='pool1')#net batch_size *16*50*64
+            print("pool_0 ", net.shape)
+            #batch_size*16*50*128
+            net = conv2d(net, 128, 3, scope='conv2')
+            net = slim.max_pool2d(net, [2, 2], scope='pool2')#batch_size *8*25*128
+            print("pool_1 ", net.shape)
+            net = conv2d(net, 256, 3, batchNormalization=True, is_training=is_training, scope='conv3')#b *8*25*256
+            net = conv2d(net, 256, 3, scope='conv4') #b*8*25*256
+            # net = custom_layers.pad2d(net, pad=(0, 1))#b*8*27*256
+            net = slim.max_pool2d(net, [2, 2], stride =[2,1], scope='pool3')#b*4*26*256
             print('pool_3', net.shape)
             #net = slim.max_pool2d(net,[2,2],stride =[2,1],padding ="SAME")
-            net = conv2d(net,4,True)#b*4*26*512
-            net = conv2d(net,5)#b*4*26*512
-            net = custom_layers.pad2d(net, pad=(0, 1))#b*4*28*512
-            net = slim.max_pool2d(net,[2,2],stride=[2,1],padding = "VALID")#b*2*27*512
-            print("pool_5", net.shape)
-            net = conv2d(net,6,True)#b*1*26*512
-            print("conv6",net.shape)
+            net = conv2d(net, 512, 3, batchNormalization=True, is_training=is_training, scope='conv5') #b*4*26*512
+            net = conv2d(net, 512, 3, scope='conv6') #b*4*26*512
+            # net = custom_layers.pad2d(net, pad=(0, 1))#b*4*28*512
+            net = slim.max_pool2d(net, [2, 2], stride =[2,1], scope='pool4') #b*2*27*512
+            print("pool_4", net.shape)
+            net = conv2d(net, 512, 2, batchNormalization=True, is_training=is_training, padding='VALID', scope='conv7') #b*1*26*512
+            print("conv7",net.shape)
 
             net = tf.squeeze(net,[1])#B*26*512
             print("squeeze: ", net.shape)
-            # net = tf.transpose(net, perm=(2,0,1)) #在pytroch中 LSTM函数顺序为seq_len,batch,input_size
-            # print("transpose: ", net.shape)
-            if width is None:
-                seq_length = self.params.seq_length
-            else:
-                seq_length = tf.cast(width/4+1,tf.int32)
-            seq_len = np.ones(self.params.batch_size) * seq_length
-            # net = BidirectionalLSTM(net,self.params.nh,self.params.nh,seq_len=seq_len,scope="BLSTM_1")#？*512*100
-            # net =tf.reshape(net,[self.params.batch_size,self.params.seq_length,self.params.nh])
-            # net = BidirectionalLSTM(net, self.params.nh, self.params.nclass, seq_len=seq_len, scope="BLSTM_2")  # ？*512*100
-            # net = tf.reshape(net, [self.params.batch_size, self.params.seq_length, self.params.nclass])
 
-            return BLSTM(net,self.params.nh,2,seq_len,self.params.nclass)
+            batch_size, length, _ = net.shape.as_list()
+            seq_len = np.full(batch_size, length) # seq_len can be used to mask out the wasted length (meanwhile returned for ctc_loss calculation)
+
+            logits = BLSTM(net, 256, 2, self.params.nclass)
+
+            return logits, seq_len
 
 
+            # # print("transpose: ", net.shape)
+            # if width is None:
+            #     seq_length = self.params.seq_length
+            # else:
+            #     seq_length = tf.cast(width/4+1,tf.int32)
+            # seq_len = np.ones(self.params.batch_size) * seq_length
 
-    def losses(self, targets,logits, seq_len,
-               scope='ssd_losses'):
-        """Define the SSD network losses.
+            # return BLSTM(net,self.params.nh,2,seq_len,self.params.nclass)
+
+
+
+    def losses(self, targets, logits, seq_len,
+               scope='ctc_losses'):
+        """Define the network losses.
         """
         with tf.name_scope(scope):
-            loss = tf.nn.ctc_loss(targets, logits, seq_len)
+            loss = tf.nn.ctc_loss(targets, logits, seq_len, time_major=False)
             cost = tf.reduce_mean(loss)
 
         return cost
--- train_mjsyth.py
+++ train_mjsyth.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from deployment import model_deploy
+# from deployment import model_deploy
 # from net import model_save as model
 import os
 slim = tf.contrib.slim
@@ -8,10 +8,37 @@ from net import model
 from dataset import read_utils
 from tensorflow.python import debug as tf_debug
 
-batch_size = 32
-num_readers = 4
-num_epochs = 2
-checkpoint_dir = './tmp/'
+
+
+flags = tf.app.flags
+flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
+flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
+flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
+flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
+
+flags.DEFINE_string("dataset", "data/h36m/", "Dataset directory.")
+flags.DEFINE_string("logdir", "logs", "Directory to save log")
+flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
+
+flags.DEFINE_string("mode", "gan", "Mode to use")
+
+flags.DEFINE_boolean("check_input", False, "Whether to check the input of D")
+flags.DEFINE_boolean("load", False, "Load existing model")
+flags.DEFINE_boolean("val_save", False, "Save images and videos when validating")
+flags.DEFINE_boolean("debug", False, "Whether to turn on debug mode")
+flags.DEFINE_boolean("verbose", False, "Whether to store verbose logs")
+
+FLAGS = flags.FLAGS
+
+if not os.path.exists(FLAGS.checkpoint_dir):
+    os.makedirs(FLAGS.checkpoint_dir)
+if not os.path.exists(FLAGS.sample_dir):
+    os.makedirs(FLAGS.sample_dir)
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
+
 
 ITEMS_TO_DESCRIPTIONS = {
     'image': 'A color image of varying height and width.',
@@ -25,81 +52,123 @@ ITEMS_TO_DESCRIPTIONS = {
 # =========================================================================== #
 def main(_):
 
-    tf.logging.set_verbosity(tf.logging.DEBUG)#设置显示的log的阈值
+    batch_size = FLAGS.batch_size
+    # num_readers = 4
+    num_epochs = FLAGS.epoch
+    checkpoint_dir = FLAGS.checkpoint_dir
 
     with tf.Graph().as_default():
 
-        deploy_config = model_deploy.DeploymentConfig()
+        # deploy_config = model_deploy.DeploymentConfig()
         # Create global_step.
-        global_step = tf.Variable(0,name='global_step',trainable=False)
+        global_step = tf.Variable(0, name='global_step', trainable=False)
 
-        file_name = os.path.join("./tfrecord", "mjtrain_690_999.tfrecords")
+        tr_file_name = os.path.join("./tfrecord", "mjtrain_train.tfrecords")
+        te_file_name = os.path.join("./tfrecord", "mjtrain_test.tfrecords")
 
-        starter_learning_rate = 0.1
+        sh_images, sh_labels, sh_length= read_utils.inputs( filename=[tr_file_name], batch_size=batch_size, num_epochs=num_epochs)
+        val_images, val_labels, val_length= read_utils.inputs( filename=[te_file_name], batch_size=batch_size, num_epochs=1000)
+
+
+        # Build Model
+        crnn = model.CRNNNet()
+        with tf.variabl_scope('crnn'):
+            logits, seq_len = crnn.net(sh_images, is_training=True)
+            tf.get_variable_scope().reuse_variables()
+            val_logits, val_seq_len = crnn.net(val_images, is_training=False)
+
+        loss = crnn.losses(sh_labels, logits, seq_len)
+        tf.summary.scalar("train/loss", loss)
+
+        val_loss = crnn.losses(val_labels, val_logits, val_seq_len)
+        # TODO: BK-tree NN search
+        decoded, log_prob = tf.nn.ctc_beam_search_decoder(val_logits, val_seq_len, merge_repeated=False)
+
+        acc = tf.reduce_mean(tf.edit_distance(tf.cast(decoded[0], tf.int32), val_labels))
+
+        val_loss_sum = tf.placeholder(tf.float32, name='val_loss_sum')
+        val_acc_sum = tf.placeholder(tf.float32, name='val_acc_sum')
+
+        tf.summary.scalar("test/val_loss", val_loss_sum)
+        tf.summary.scalar("test/edit_distance", val_acc_sum)
+
+
+
+        starter_learning_rate = FLAGS.learning_rate
         learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,
                                                    100000, 0.96, staircase=True)
-        tf.summary.scalar("learning_rate",learning_rate)
-        sh_images, sh_labels, sh_length= read_utils.inputs( filename = file_name,batch_size=batch_size,
-                                num_epochs=num_epochs)
+        tf.summary.scalar("train/learning_rate",learning_rate)
+
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):        
+            optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss=loss, global_step=global_step)
+
+        # Start Training
+        with tf.Session(config=config) as sess:
+            save = tf.train.Saver(max_to_keep=10)
+
+            if not FLAGS.load:
+                init_op = tf.group(tf.global_variables_initializer(),
+                                   tf.local_variables_initializer())
+
+                sess.run(init_op)            # Start input enqueue threads.
+            else:
+
+                # ckpt_file = 'model.ckpt-' + FLAGS.ckpt_step
+                ckpt_path = os.path.join(FLAGS.checkpoint_dir, FLAGS.ckpt_file)
+                save.restore(sess, ckpt_path)
+                sess.run(tf.local_variables_initializer())
 
 
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+            #sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+            merged = tf.summary.merge(tf.get_collection(tf.GraphKeys.SUMMARIES, scope='train/*'))
+            val_merged = tf.summary.merge(tf.get_collection(tf.GraphKeys.SUMMARIES, scope='test/*'))
 
+            file_writer = tf.summary.FileWriter(FLAGS.logdir, sess.graph)
 
-        crnn = model.CRNNNet()
-        logits, inputs, seq_len, W, b = crnn.net(sh_images)
+            try:
 
-        cost = crnn.losses(sh_labels,logits, seq_len)
-        optimizer = tf.train.AdadeltaOptimizer(learning_rate = learning_rate).minimize(loss=cost,global_step=global_step)
-        tf.summary.scalar("cost",cost)
+                while not coord.should_stop():
+                    start_time = time.time()
 
+                    _, merged_t, tr_loss, lr, step = sess.run([optimizer, merged, loss, learning_rate, global_step])
 
-        decoded, log_prob = tf.nn.ctc_beam_search_decoder(logits, seq_len, merge_repeated=False)
+                    duration = time.time() - start_time
 
+                    print("loss", tr_loss, "time", duration)
+                    file_writer.add_summary(merged_t, step)
 
-        acc = tf.reduce_mean(tf.edit_distance(tf.cast(decoded[0], tf.int32), sh_labels))
-        tf.summary.scalar("edit_distance",acc)
+                    # Print an overview fairly often.
+                    if step % 1000 == 0:
+                        #######################################################
 
-        sess = tf.Session()
+                        val_loss_s, val_acc_s = 0, 0
+                        for ite in range(FLAGS.sample_size):
+                            te_loss, te_acc = sess.run([val_loss, acc])
+                            val_loss_s += te_loss
+                            val_acc_s += te_acc
+                        val_loss_s /= FLAGS.sample_size
+                        val_acc_s /= FLAGS.sample_size
 
-        save = tf.train.Saver(max_to_keep=2)
-        if tf.train.latest_checkpoint(checkpoint_dir) is None:
-            init_op = tf.group(tf.global_variables_initializer(),
-                               tf.local_variables_initializer())
-            sess.run(init_op)            # Start input enqueue threads.
-        else:
-            save.restore(sess,tf.train.latest_checkpoint(checkpoint_dir))
-            sess.run(tf.local_variables_initializer())
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-        #sess = tf_debug.LocalCLIDebugWrapperSession(sess)
-        merged = tf.summary.merge_all()
-        file_writer = tf.summary.FileWriter('./tmp/my-model', sess.graph)
+                        print('Step %d: loss %.3f acc %.3f (%.3f sec)' % (step, val_loss_s, val_acc_s, duration))
 
-        try:
-            step = global_step
-            while not coord.should_stop():
-                start_time = time.time()
+                        # Add summary
+                        val_sum = sess.run(val_merged, feed_dict={val_loss_sum: val_loss_s, val_acc_sum: val_acc_s})
+                        file_writer.add_summary(val_sum, step)
 
-                _,merged_t,val_cost, val_ler, lr, step = sess.run([optimizer,merged,cost, acc, learning_rate, global_step])
+                        save.save(sess, os.path.join(FLAGS.checkpoint_dir, 'model.ckpt'), global_step=step)
+                    
+            except tf.errors.OutOfRangeError:
+                print('Done training for %d epochs, %d steps.' % (num_epochs, step))
+            finally:
+                # When done, ask the threads to stop.
+                coord.request_stop()
 
-                duration = time.time() - start_time
+                # Wait for threads to finish.
+            coord.join(threads)
 
-                print("cost", val_cost)
-                file_writer.add_summary(merged_t,step)
-                # Print an overview fairly often.
-                if step % 10 == 0:
-                    print('Step %d:  acc %.3f (%.3f sec)' % (step,val_ler,duration))
-                    save.save(sess,"./tmp/crnn-model.ckpt",global_step = global_step)
-                step += 1
-        except tf.errors.OutOfRangeError:
-            print('Done training for %d epochs, %d steps.' % (num_epochs, step))
-        finally:
-            # When done, ask the threads to stop.
-            coord.request_stop()
-
-            # Wait for threads to finish.
-        coord.join(threads)
-        sess.close()
 if __name__ == '__main__':
     tf.app.run()
 
